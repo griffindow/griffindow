2e03bb54a35f08eb1553710939b2ef63ebec9624
Griffin <griffin@griffindow.com>
2025-08-24 08:18:13 -0700

    fix: set account

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,7 +5,7 @@ import { CdkStack } from '../lib/cdk-stack';
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION ?? 'us-east-1',
+    account: '205930649223',
+    region: 'us-east-1',
   },
 });
